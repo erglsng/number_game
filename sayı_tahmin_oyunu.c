1c56c62129f93cd7202c9d3700f7d2be27a93cd5
erglsng <75937110+erglsng@users.noreply.github.com>
2021-04-06 22:17:37 +0300

    Update sayı_tahmin_oyunu.c

--- sayı_tahmin_oyunu.c
+++ sayı_tahmin_oyunu.c
@@ -7,39 +7,64 @@ main(){
 	
 	int  rastgeleSayi = 0, girilenSayi = 0, denemeSayisi = 0; 
 	
-       /*srand(time(NULL));
-	rastgeleSayi = rand() % 10;  1 ile 10 arası rastgele sayı üretmesi için bilgisayara verilmesi gereken komut*/
-	
+
 	srand(time(NULL));
 	
 	rastgeleSayi = rand() % 10;
-do{  
-	printf(" 1-10 Arasi sayi tutulmustur:\n");
+	
+	do{
+		
+			printf(" 1-10 Arasi sayi tutulmustur:\n");
 	printf(" Tahmininiz Nedir?:");
 	scanf("%d",&girilenSayi);
 	
-if(rastgeleSayi < girilenSayi){
-	printf("Girdiginiz sayi buyuktur daha kucuk bir sayi girin");
-	printf("\n\n");
+	/*if(girilenSayi>10){
+		printf("10'dan buyuktur. Sadece 1-10 arasinda sayi giriniz\n");
+	}
+	else if(girilenSayi<10)
+	{
+		printf("1'den kucuktur.Sadece 1-10 arasinda sayi giriniz\n"); GİRİLEN SAYININ 1-10 ARASINDA OLUP OLMADIĞINI KONTROL EDEN İF-ELSE İF KOMUTLARI
+		
+	}*/
+	if(girilenSayi>10){
+	printf("10'dan buyuktur. Sadece 1-10 arasinda sayi giriniz\n");
+	}
+	else if(girilenSayi<10)
+	{
+		printf("1'den kucuktur.Sadece 1-10 arasinda sayi giriniz\n");
+		
+	}
+	
+	if(rastgeleSayi < girilenSayi){
+		printf("Girdiginiz sayi buyuktur daha kucuk bir sayi girin");
+			printf("\n\n");
 
 		}
-else if(rastgeleSayi > girilenSayi){
-	printf("Girdiginiz sayi kucuktur daha buyuk bir sayi girin");
-        printf("\n\n");
+		
+	else if(rastgeleSayi > girilenSayi){
+				printf("Girdiginiz sayi kucuktur daha buyuk bir sayi girin");
+printf("\n\n");
 
+	
+	
+	
 	}
 	denemeSayisi++;
 	
 
 
-} while(rastgeleSayi != girilenSayi);
+	} 	while(rastgeleSayi != girilenSayi);
+	
 
-        printf("Tebrikler. %d. seferde bildiniz",denemeSayisi);
+   printf("Tebrikler. %d. seferde bildiniz",denemeSayisi);
    
 	 
    
 }
     
+   
+
+	
    
 
 	
